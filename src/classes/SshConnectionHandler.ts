39c2418d9a2358b3f270831b6d3512d25ba755ac
Dan Schultz <slifty@gmail.com>
2022-01-17 16:27:26 -0500

    Add always-accepting authentication
    As described in the [SSH2
    spec](https://datatracker.ietf.org/doc/html/rfc4252#section-5.2) the
    client is able to pass `none` as the authentication type and we are
    supposed to respond with a list of valid authentication methods for our
    service.
    
    We plan on requiring a bearer token to be passed as the password, and in
    particular we aren't going to be using keys for authentication.
    
    For now, however, we're going to just accept any password provided.
    
    Issue #3

--- src/classes/SshConnectionHandler.ts
+++ src/classes/SshConnectionHandler.ts
@@ -1,6 +1,28 @@
-export class SshConnectionHandler {
+import { logger } from '../logger';
+import type { AuthContext } from 'ssh2';
 
-  public onAuthentication = (): void => {};
+export class SshConnectionHandler {
+  /**
+   * See: Authentication Requests
+   * https://datatracker.ietf.org/doc/html/rfc4252#section-5
+   */
+  public onAuthentication = (authContext: AuthContext): void => {
+    logger.debug('Authentication request recieved.', {
+      username: authContext.username,
+      method: authContext.method,
+    });
+    switch (authContext.method) {
+      case 'password':
+        authContext.accept();
+        return;
+      case 'none':
+      default:
+        authContext.reject(
+          ['password'],
+        );
+        return;
+    }
+  };
 
   public onClose = (): void => {};
 
