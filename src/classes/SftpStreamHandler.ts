8398e84b52b1ce176b9a2efebb4b935a93aea803
Dan Schultz <slifty@gmail.com>
2022-01-17 21:38:53 -0500

    Start sftp sessions on request
    If an sftp event is triggered via an ssh session we need to begin an
    sftp stream.
    
    This creates the stream by accepting the request and then stubs out the
    handlers (similar to how we stubbed out the SSH handlers).
    
    None of the sftp event handlers actually do anything yet, but by
    stubbing them out we know what needs to be populated.
    
    Issue #3

---
+++ src/classes/SftpStreamHandler.ts
@@ -0,0 +1,37 @@
+export class SftpStreamHandler {
+  public openHandler = (): void => {};
+
+  public readHandler = (): void => {};
+
+  public writeHandler = (): void => {};
+
+  public fstatHandler = (): void => {};
+
+  public fsetStatHandler = (): void => {};
+
+  public closeHandler = (): void => {};
+
+  public openDirHandler = (): void => {};
+
+  public readDirHandler = (): void => {};
+
+  public lstatHandler = (): void => {};
+
+  public statHandler = (): void => {};
+
+  public removeHandler = (): void => {};
+
+  public rmDirHandler = (): void => {};
+
+  public realPathHandler = (): void => {};
+
+  public readLinkHandler = (): void => {};
+
+  public setStatHandler = (): void => {};
+
+  public mkDirHandler = (): void => {};
+
+  public renameHandler = (): void => {};
+
+  public symLinkHandler = (): void => {};
+}
