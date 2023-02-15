54d87ae81ead4423f7bc4aacdd39533e02e90259
Dan Schultz <slifty@gmail.com>
2023-02-15 15:22:45 -0500

    Migrate to 0.5.0 of permanent SDK
    The new Permanent SDK has support for the creation of archive records.
    It also renames `Record` to `ArchiveRecord` (Record had a naming
    collision with the built in "Record" typescript utility type).  This
    commit should not involve any functional changes, it just updates our
    names to refer to "Archive Records" across the board.
    
    Issue #77

---
+++ src/utils/getOriginalFileForArchiveRecord.ts
@@ -0,0 +1,15 @@
+import { DerivativeType } from '@permanentorg/sdk';
+import type {
+  File,
+  ArchiveRecord,
+} from '@permanentorg/sdk';
+
+export const getOriginalFileForArchiveRecord = (archiveRecord: ArchiveRecord): File => {
+  const originalFile = archiveRecord.files.find(
+    (file) => file.derivativeType === DerivativeType.Original,
+  );
+  if (!originalFile) {
+    throw Error(`Permanent does not have an original file for Archive Record ${archiveRecord.id}`);
+  }
+  return originalFile;
+};
