f2a21a1cdcdf42686cab64b937ace3f374095118
Dan Schultz <slifty@gmail.com>
2025-10-06 11:49:28 -0400

    Enable `no-negated-condition`
    This was recently added to the default rule set but we were violating it
    at the time.  This re-enables the rule alongside the necessary changes
    to code.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -71,7 +71,6 @@ export default defineConfig([
 			"logical-assignment-operators": "off",
 			"@typescript-eslint/explicit-function-return-type": "off",
 			"@typescript-eslint/prefer-nullish-coalescing": "off",
-			"no-negated-condition": "off",
 		},
 	},
 	{
